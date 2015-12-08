04ff78243c63ada8a3a9b063b784f4304725c13b
singoon <singoon.he@gmail.com>
2015-12-09 00:44:37 +0800

    add free ShaderCode cache data in ResourceManager constructor

---
+++ Peach3D/Core/Peach3DShaderCode.cpp
@@ -0,0 +1,48 @@
+//
+//  Peach3DShaderCode.cpp
+//  Peach3DLib
+//
+//  Created by singoon.he on 7/1/15.
+//  Copyright (c) 2015 singoon.he. All rights reserved.
+//
+
+#include "Peach3DShaderCode.h"
+
+namespace Peach3D
+{
+    std::map<std::string, ShaderCodeData> ShaderCode::mShaderMap;
+    std::map<std::string, std::vector<ProgramUniform>> ShaderCode::mUniformsMap;
+    std::map<UniformDataType, uint> ShaderCode::mUniformsBitsMap;
+    std::map<std::string, UniformNameType> ShaderCode::mUniformsNamesMap;
+    
+    uint ShaderCode::getUniformFloatBits(UniformDataType type)
+    {
+        if (mUniformsBitsMap.empty()) {
+            mUniformsBitsMap[UniformDataType::eFloat] = 1;
+            mUniformsBitsMap[UniformDataType::eVector2] = 2;
+            mUniformsBitsMap[UniformDataType::eVector3] = 3;
+            mUniformsBitsMap[UniformDataType::eVector4] = 4;
+            mUniformsBitsMap[UniformDataType::eMatrix4] = 16;
+        }
+        return mUniformsBitsMap[type];
+    }
+    
+    UniformNameType ShaderCode::getUniformNameType(const std::string& name)
+    {
+        if (mUniformsNamesMap.empty()) {
+            mUniformsNamesMap["pd_viewRect"] = UniformNameType::eViewRect;
+            mUniformsNamesMap["pd_showRect"] = UniformNameType::eShowRect;
+            mUniformsNamesMap["pd_anRot"] = UniformNameType::eAnRot;
+            mUniformsNamesMap["pd_patShowRect"] = UniformNameType::ePatShowRect;
+            mUniformsNamesMap["pd_patAnRot"] = UniformNameType::ePatAnRot;
+            mUniformsNamesMap["pd_diffuse"] = UniformNameType::eDiffuse;
+            mUniformsNamesMap["pd_uvRect"] = UniformNameType::eUVRect;
+            mUniformsNamesMap["pd_texEffect"] = UniformNameType::eTexEffect;
+            
+            mUniformsNamesMap["pd_projMatrix"] = UniformNameType::eProjMatrix;
+            mUniformsNamesMap["pd_viewMatrix"] = UniformNameType::eViewMatrix;
+            mUniformsNamesMap["pd_modelMatrix"] = UniformNameType::eModelMatrix;
+        }
+        return mUniformsNamesMap[name];
+    }
+}
