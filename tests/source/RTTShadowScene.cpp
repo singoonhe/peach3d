97c4efe0f4dc61ecf5527e4285e66f9dd0c065be
singoon <singoon.he@gmail.com>
2016-03-31 17:11:42 +0800

    fix RTT sample title and describe

--- tests/source/RTTShadowScene.cpp
+++ tests/source/RTTShadowScene.cpp
@@ -22,23 +22,26 @@ void RTTSample::init(Widget* parentWidget)
 {
     auto screenSize = LayoutManager::getSingleton().getScreenSize();
     // only set title here
-    mTitle = "Direction Light";
-    mDesc = "*.pmt using vertex normal, *.obj using face normal";
+    mTitle = "RTT(render to texture) sample";
+    mDesc = "render 3D world to texture, used on sprite";
     
     // create a girl
     auto girlMesh = ResourceManager::getSingleton().addMesh("girl.pmt");
-    auto texNode = SceneManager::getSingleton().getRootSceneNode()->createChild(Vector3(0.f, -5.f, 0.f));
-    texNode->attachMesh(girlMesh);
+    auto texNode1 = SceneManager::getSingleton().getRootSceneNode()->createChild(Vector3(-6.f, -5.f, 0.f));
+    texNode1->attachMesh(girlMesh);
+    auto texNode2 = SceneManager::getSingleton().getRootSceneNode()->createChild(Vector3(6.f, -5.f, 0.f));
+    texNode2->attachMesh(girlMesh);
     // rotate repeat
-    texNode->runAction(Repeat::createForever(RotateBy3D::create(Vector3(0.0f, DEGREE_TO_RADIANS(360.0f), 0.0f), 5.0f)));
+    texNode1->runAction(Repeat::createForever(RotateBy3D::create(Vector3(0.0f, DEGREE_TO_RADIANS(360.0f), 0.0f), 5.0f)));
+    texNode2->runAction(Repeat::createForever(RotateBy3D::create(Vector3(0.0f, -DEGREE_TO_RADIANS(360.0f), 0.0f), 5.0f)));
     
     // create RTT
     int rw = screenSize.x / 5, rh = screenSize.y / 5;
     ITexture* rttT = ResourceManager::getSingleton().createRenderTexture(rw, rh);
     rttT->setBeforeRenderingFunc([]{
         auto activeCamera = SceneManager::getSingleton().getActiveCamera();
-        activeCamera->setPosition(Vector3(0.f, 3.5f, 3.f));
-        activeCamera->lockToPosition(Vector3(0.f, 3.5f, 0.f));
+        activeCamera->setPosition(Vector3(-6.f, 3.5f, 3.f));
+        activeCamera->lockToPosition(Vector3(-6.f, 3.5f, 0.f));
         IRender::getSingleton().setRenderClearColor(Color4(1.f, 0.f, 0.f));
     });
     rttT->setAfterRenderingFunc([]{
